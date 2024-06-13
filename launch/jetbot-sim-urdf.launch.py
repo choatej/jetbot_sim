e997a13ff1f106ce010140e863509df60510860d
Jon Choate <jon.choate@takeoff.com>
2024-06-13 13:51:45 -0400

    feat: initial commit

---
+++ launch/jetbot-sim-urdf.launch.py
@@ -0,0 +1,100 @@
+import os
+import random
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_prefix
+
+
+def generate_launch_description():
+
+    package_name = "jetbot_sim"
+    install_dir = get_package_prefix(package_name)
+
+    # This is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(package_name, 'models')
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + \
+            ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = install_dir + \
+            "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + \
+            ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={"verbose": "false", 'pause': 'true'}.items(),
+    )
+
+    robot_model_path = os.path.join(
+        get_package_share_directory(package_name))
+
+    urdf_file = os.path.join(robot_model_path, 'urdf', 'my-jetbot.urdf')
+    with open(urdf_file, 'r') as f:
+        doc = f.read()
+    params = {'robot_description': doc}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0.0, 0.0, 0.2]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0.0]
+    # Base Name or robot
+    robot_name = "jetbot"
+
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   robot_name,
+                   '-x', str(position[0]), '-y', str(position[1]
+                                                     ), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]
+                                                        ), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description'
+                   ]
+    )
+
+    # RVIZ Configuration
+    rviz_config_file = os.path.join(get_package_share_directory(package_name), 'rviz', 'config.rviz')
+    print(f'rviz config dir: {rviz_config_file}')
+
+    print('creating rviz2 node')
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_file]
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_robot,
+        rviz_node,
+    ])
